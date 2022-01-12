2fa570371fa0c78a97a121f1f56ba53ea14e4331
huxiaohui <826564596@qq.com>
2022-01-12 18:14:53 +0800

    feat:冒泡和各种优化

--- src/4. 寻找两个正序数组的中位数/index.ts
+++ src/4. 寻找两个正序数组的中位数/index.ts
@@ -1,3 +1,14 @@
+/**
+ * 最暴力简单的解法：通过合并两个数组，排序，判断数组长度奇偶输出。时间复杂度达到了 O(n^2)
+ * @param nums1 
+ * @param nums2 
+ * @returns number
+ */
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    return 1;
+    let number: number[] = [...nums1, ...nums2].sort((a, b) => a - b)
+    let l = number.length;
+    return l % 2 ? number[parseInt(String(l / 2))] : (number[l / 2 - 1] + number[l / 2]) / 2;
 };
+
+let nums1 = [], nums2 = [3]
+console.log(findMedianSortedArrays(nums1, nums2))
---
+++ src/4. 寻找两个正序数组的中位数/index1.ts
@@ -0,0 +1,27 @@
+/**
+ * 使用数组的归并排序
+ * @param nums1 
+ * @param nums2 
+ * @returns number
+ */
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+    let totalLen = nums1.length + nums2.length;
+    let totalArr: number[] = [];
+    let i1 = 0, i2 = 0;
+    while (i1 + i2 < totalLen) {
+        let val1 = nums1[i1];
+        let val2 = nums2[i2];
+        if (val2 == null || val1 < val2) {
+            totalArr.push(val1);
+            i1++;
+        } else {
+            totalArr.push(val2);
+            i2++;
+        }
+
+    }
+
+    return totalLen % 2 ? totalArr[parseInt(String(totalLen / 2))] : (totalArr[totalLen / 2 - 1] + totalArr[totalLen / 2]) / 2;
+};
+let num1 = [1, 2, 3, 4], num2 = [2, 4, 5, 6]
+console.log(findMedianSortedArrays(num1, num2))
