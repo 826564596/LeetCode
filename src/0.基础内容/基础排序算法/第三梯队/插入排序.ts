51467b1e40ff6e2880abea2c365bd55ae3676fc1
huxiaohui <826564596@qq.com>
2022-01-14 09:23:31 +0800

    feat:插入排序 和 选择排序

---
+++ src/0.基础内容/基础排序算法/第三梯队/插入排序.ts
@@ -0,0 +1,22 @@
+// 时间复杂度: O(n^2); 原始数组越有序，性能越好
+//快速排序，维护一个有序区，把元素一个一个插入到有序区的适当位置，直到所有元素有序
+function insertSort(array: number[]) {
+    // 从index==1开始即将第一个元素作为一个有序区,所以拿第二个元素比较
+    for (let index = 1; index < array.length; index++) {
+        let t = array[index];
+        let j = index - 1; //表示有序区需要比较的次数
+        //插入排序优化
+        for (; j >= 0 && array[j] > t; j--) {
+            array[j + 1] = array[j];
+        }
+        //每次都要插入到j+1 的地方，因为会将j减到 index-1-1的地方
+        array[j + 1] = t;
+    }
+    return array;
+}
+
+let array = [10, 6, 5, 4, 3, 1, 6, 7, 8];
+
+console.time("插入排序时间");
+console.log(JSON.stringify(insertSort(array)));
+console.timeEnd("插入排序时间");
---
+++ src/0.基础内容/基础排序算法/第三梯队/选择排序.ts
@@ -0,0 +1,25 @@
+// 时间复杂度: O(n^2); 原始数组越无序，性能越好
+// 选择排序,先规定左边为最小每轮选择最小者,直接交换到数组最左边
+function sort(array: number[]) {
+    let num = 0;
+    for (let index = 0; index < array.length - 1; index++) {
+        let minIndex = index;
+        for (let j = index + 1; j < array.length; j++) {
+            num++;
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        let t = array[minIndex];
+        array[minIndex] = array[index];
+        array[index] = t;
+    }
+    console.log(num);
+    return array;
+}
+
+let array10 = [10, 6, 5, 4, 3, 1, 6, 7, 8];
+
+console.time("选择排序时间");
+console.log(JSON.stringify(sort(array10)));
+console.timeEnd("选择排序时间");
